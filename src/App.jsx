fdee5083e7a87368558afcc02e5a5583d5a88992
Bijay kumar sah <vijaykumarsah293@gmail.com>
2023-01-21 10:27:31 +0530

    final messge

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Home from './Pages/Home/Home';
+import Orders from './Pages/Orders/Orders';
+import Products from './Pages/Products/Products';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Delievery from './Pages/Delievery/Delievery';
+import Navbar from './Components/Navbar/Navbar';
+function App() {
+  return (
+   <div>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/orders" element={<Orders/>}/>
+            <Route path="/products" element={<Products/>}/>
+            <Route path="/Delievery" element={<Delievery/>}/>
+         </Routes>
+      </BrowserRouter>
+   </div>
+  );
+}
+
+export default App;
